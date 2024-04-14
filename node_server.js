f9bbff2a5bab75a5b727fb747b1f9739c2580168
SwordJack <49801226+SwordJack@users.noreply.github.com>
2024-04-13 23:30:35 -0500

    React: Load content from build folder.

--- node_server.js
+++ node_server.js
@@ -5,12 +5,12 @@ var favicon = require('serve-favicon')
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')))
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
 const port = process.env.PORT || 3000;
