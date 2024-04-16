1360ba4b75b2630a8937a725abe409f337f04dcc
kierannp <nehilkieran@gmail.com>
2024-04-15 20:08:20 -0500

    Reverting button

--- src/screens/Experiments/components/Button/Button.jsx
+++ src/screens/Experiments/components/Button/Button.jsx
@@ -6,25 +6,25 @@ Please share your feedback here: https://form.asana.com/?k=uvp-HPgd3_hyoXRBw1IcN
 import PropTypes from "prop-types";
 import React from "react";
 import { useReducer } from "react";
-import { Add12 } from "../../icons/Add12";
-import { Add2 } from "../../icons/Add2";
-import { Search } from "../../icons/Search";
-import { IconTrash2 } from "../../icons/IconTrash2";
-import { IconSliders } from "../../icons/IconSliders";
+import { Add } from "../../icons/Add";
+import { Add10 } from "../../icons/Add10";
+import { AddWrapper } from "../AddWrapper";
 import "./style.css";
 
 export const Button = ({
   icon = true,
   resizer = false,
   buttonText = "Button",
+  disabled = false,
   style,
   type,
   size,
   stateProp,
   className,
+  override = <Add className="add-56" color="#161616" />,
   iconClassName,
+  icon1 = <Add className="add-56" color="#FFFFFF" />,
   onClick,
-  isModal = false,
 }) => {
   const [state, dispatch] = useReducer(reducer, {
     style: style || "primary",
@@ -33,19 +33,22 @@ export const Button = ({
     state: stateProp || "enabled",
   });
 
+  if(state.state!=="disabled" && disabled){
+    dispatch("button_disabled");
+  } else if (state.state === "disabled" && !disabled) {
+    dispatch("button_enabled");
+  }
+
   return (
     <button
-      className={isModal ? `button ${state.state} ${state.style} ${state.size} ${state.type} ${className}` : `button state-0-${state.state} ${state.style} ${state.size} ${state.type} ${className}`}
+      className={`button ${state.state} ${state.style} ${state.size} ${state.type} ${className}`}
       onMouseEnter={() => {
         dispatch("mouse_enter");
       }}
       onMouseLeave={() => {
         dispatch("mouse_leave");
       }}
-      onClick={() => {
-        dispatch("click");
-        onClick && onClick();
-      }}
+      onClick={onClick}
     >
       {((state.size === "expressive" && state.state === "hover") ||
         (state.size === "extra-large" && state.state === "hover") ||
@@ -66,72 +69,7 @@ export const Button = ({
           {state.type === "text-icon" && <div className="text-wrapper-2">{buttonText}</div>}
 
           {state.type === "icon-only" && ["primary", "secondary", "tertiary"].includes(state.style) && (
-            <div className="icon">
-              {iconClassName === "Add2" && (
-                <Add2
-                  className={`${["extra-large", "large", "medium", "small"].includes(state.size) && "class-2"} ${
-                    state.size === "expressive" && "add-12"
-                  }`}
-                  color={
-                    state.style === "tertiary" && state.state === "enabled"
-                      ? "#0F62FE"
-                      : state.state === "disabled" && ["primary", "secondary"].includes(state.style)
-                      ? "#8D8D8D"
-                      : state.style === "tertiary" && state.state === "disabled"
-                      ? "#C6C6C6"
-                      : "white"
-                  }
-                />)
-              }
-              {iconClassName === "Add" && (
-                    <Add2
-                    className={`${["extra-large", "large", "medium", "small"].includes(state.size) && "class-2"} ${
-                      state.size === "expressive" && "add-12"
-                    }`}
-                    color={
-                      state.style === "tertiary" && state.state === "enabled"
-                        ? "#0F62FE"
-                        : state.state === "disabled" && ["primary", "secondary"].includes(state.style)
-                        ? "#8D8D8D"
-                        : state.style === "tertiary" && state.state === "disabled"
-                        ? "#C6C6C6"
-                        : "white"
-                    }
-                  />)
-              }
-              {iconClassName === "IconTrash2" && (
-                  <IconTrash2
-                  className={`${["extra-large", "large", "medium", "small"].includes(state.size) && "class-2"} ${
-                    state.size === "expressive" && "add-12"
-                  }`}
-                  color={
-                    state.style === "tertiary" && state.state === "enabled"
-                      ? "#0F62FE"
-                      : state.state === "disabled" && ["primary", "secondary"].includes(state.style)
-                      ? "#8D8D8D"
-                      : state.style === "tertiary" && state.state === "disabled"
-                      ? "#C6C6C6"
-                      : "white"
-                  }
-                />
-              )}
-              {iconClassName === "IconSliders" && (
-                  <IconSliders
-                  className={`${["extra-large", "large", "medium", "small"].includes(state.size) && "class-2"} ${
-                    state.size === "expressive" && "add-12"
-                  }`}
-                  color={
-                    state.style === "tertiary" && state.state === "enabled"
-                      ? "#0F62FE"
-                      : state.state === "disabled" && ["primary", "secondary"].includes(state.style)
-                      ? "#8D8D8D"
-                      : state.style === "tertiary" && state.state === "disabled"
-                      ? "#C6C6C6"
-                      : "white"
-                  }
-                />
-              )}
-            </div>
+            <div className="add-wrapper">{override}</div>
           )}
 
           {state.size === "two-x-large" && state.style === "ghost" && <div className="icon-spacer" />}
@@ -147,8 +85,7 @@ export const Button = ({
               <>
                 {icon && (
                   <div
-                    style={{background: 'transparent', mixBlendMode: 'multiply'}}
-                    className={`icon-3 ${
+                    className={`icon-4 ${
                       ["danger-primary", "danger-tertiary", "primary", "secondary", "tertiary"].includes(state.style)
                         ? iconClassName
                         : undefined
@@ -175,67 +112,32 @@ export const Button = ({
                       <>
                         <>
                           {icon && (
-                            <Add2
-                              className={`${state.size === "expressive" ? "add-12" : "class-2"}`}
-                              color={
-                                (state.state === "active" && state.style === "danger-primary") ||
-                                (state.state === "active" && state.style === "danger-tertiary") ||
-                                (state.state === "active" && state.style === "primary") ||
-                                (state.state === "active" && state.style === "secondary") ||
-                                (state.state === "active" && state.style === "tertiary") ||
-                                (state.state === "enabled" && state.style === "danger-primary") ||
-                                (state.state === "enabled" && state.style === "primary") ||
-                                (state.state === "enabled" && state.style === "secondary") ||
-                                (state.state === "focus" && state.style === "danger-primary") ||
-                                (state.state === "focus" && state.style === "danger-tertiary") ||
-                                (state.state === "focus" && state.style === "primary") ||
-                                (state.state === "focus" && state.style === "secondary") ||
-                                (state.state === "focus" && state.style === "tertiary") ||
-                                (state.state === "hover" && state.style === "danger-primary") ||
-                                (state.state === "hover" && state.style === "danger-tertiary") ||
-                                (state.state === "hover" && state.style === "primary") ||
-                                (state.state === "hover" && state.style === "secondary") ||
-                                (state.state === "hover" && state.style === "tertiary")
-                                  ? "white"
-                                  : (state.state === "enabled" && state.style === "ghost") ||
-                                    (state.state === "enabled" && state.style === "tertiary") ||
-                                    (state.state === "focus" && state.style === "ghost")
-                                  ? "#0F62FE"
-                                  : state.style === "danger-tertiary" && state.state === "enabled"
-                                  ? "#DA1E28"
-                                  : state.state === "disabled" &&
-                                    ["danger-primary", "primary", "secondary"].includes(state.style)
-                                  ? "#8D8D8D"
-                                  : state.state === "disabled" &&
-                                    ["danger-tertiary", "ghost", "tertiary"].includes(state.style)
-                                  ? "#C6C6C6"
-                                  : state.style === "ghost" && ["active", "hover"].includes(state.state)
-                                  ? "#0043CE"
-                                  : undefined
-                              }
-                            />
+                            <>
+                              <>
+                                {["danger-primary", "danger-tertiary", "primary", "secondary", "tertiary"].includes(
+                                  state.style
+                                ) && <>{icon1}</>}
+
+                                {state.style === "ghost" && (
+                                  <Add
+                                    className="add-56"
+                                    color={
+                                      state.state === "disabled"
+                                        ? "#C6C6C6"
+                                        : ["active", "hover"].includes(state.state)
+                                        ? "#0043CE"
+                                        : "#0F62FE"
+                                    }
+                                  />
+                                )}
+                              </>
+                            </>
                           )}
                         </>
                       </>
                     )}
 
-                    {((state.size === "extra-large" && state.state === "disabled" && state.type === "icon-only") ||
-                      (state.size === "large" && state.state === "disabled" && state.type === "icon-only") ||
-                      (state.size === "medium" && state.state === "disabled" && state.type === "icon-only") ||
-                      (state.size === "small" && state.state === "disabled" && state.type === "icon-only") ||
-                      (state.state === "active" && state.type === "icon-only") ||
-                      (state.state === "enabled" && state.type === "icon-only") ||
-                      (state.state === "focus" && state.type === "icon-only") ||
-                      (state.state === "hover" && state.type === "icon-only")) && (
-                      <Add2
-                        className={`${state.size === "expressive" ? "add-12" : "class-2"}`}
-                        color={state.state === "disabled" ? "#C6C6C6" : "#161616"}
-                      />
-                    )}
-
-                    {state.type === "icon-only" && state.state === "disabled" && state.size === "expressive" && (
-                      <Add12 className="add-12" />
-                    )}
+                    {state.type === "icon-only" && <>{override}</>}
                   </div>
                 )}
               </>
@@ -251,7 +153,7 @@ export const Button = ({
             <>
               <>
                 {icon && (
-                  <div className="add-wrapper">
+                  <div className="icon-2">
                     {icon && (
                       <>
                         <>
@@ -263,8 +165,8 @@ export const Button = ({
                             state.state === "enabled" ||
                             state.state === "focus" ||
                             state.state === "hover") && (
-                            <iconClassName
-                              className={`${state.size === "expressive" ? "add-12" : "class-2"}`}
+                            <Add
+                              className={`${state.size === "expressive" ? "add-10" : "add-56"}`}
                               color={
                                 state.style === "ghost" && ["enabled", "focus"].includes(state.state)
                                   ? "#0F62FE"
@@ -279,7 +181,7 @@ export const Button = ({
                             />
                           )}
 
-                          {state.state === "disabled" && state.size === "expressive" && <iconClassName className="icon-class" />}
+                          {state.state === "disabled" && state.size === "expressive" && <Add10 className="add-10" />}
                         </>
                       </>
                     )}
@@ -296,11 +198,10 @@ export const Button = ({
           <button className="button-wrapper">
             <div className="text-wrapper-3">{buttonText}</div>
           </button>
-          <>{icon && <div className="icon-2">{icon && <Add2 />}</div>}</>
-          <>{icon && <div className="icon-2">{icon && <Add2 />}</div>}</>
+          <>{icon && <div className="icon-3">{icon && <AddWrapper />}</div>}</>
+          <>{icon && <div className="icon-3">{icon && <AddWrapper />}</div>}</>
         </>
       )}
-    
     </button>
   );
 };
@@ -312,25 +213,21 @@ function reducer(state, action) {
         ...state,
         state: "hover",
       };
+
     case "mouse_leave":
       return {
         ...state,
         state: "enabled",
       };
-    case "click":
-      return {
-        ...state,
-        state: "active",
-      };
     case "button_disabled":
       return {
-          ...state,
-          state: "disabled",
+        ...state,
+        state: "disabled",
       };
     case "button_enabled":
       return {
-          ...state,
-          state: "enabled",
+        ...state,
+        state: "enabled",
       };
   }
 
@@ -353,5 +250,4 @@ Button.propTypes = {
   type: PropTypes.oneOf(["icon-only", "text-icon"]),
   size: PropTypes.oneOf(["large", "two-x-large", "extra-large", "expressive", "small", "medium"]),
   stateProp: PropTypes.oneOf(["active", "enabled", "focus", "hover", "skeleton", "disabled"]),
-  onClick: PropTypes.func,
 };
