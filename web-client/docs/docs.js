2fed15f6b87a5efd509d24f499514fa55ee72b90
SwordJack <49801226+SwordJack@users.noreply.github.com>
2025-01-20 19:22:11 -0600

    Render the Terms of Service and Privacy Policy pages.

--- web-client/docs/docs.js
+++ web-client/docs/docs.js
@@ -1,11 +1,39 @@
 
+/**
+ * Get Params from HTTP Header.
+ * @returns An object containing param name and value as a dictionary.
+ */
+function GetRequest() {
+  var url = location.search; // Get params from http header.
+  //   console.log(url);
+  var theRequest = new Object();
+  if (url.indexOf("?") != -1) {
+    var str = url.substr(1);
+    strs = str.split("&");
+    for (var i = 0; i < strs.length; i++) {
+      theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
+    }
+  }
+  //   console.log(theRequest);
+  return theRequest;
+}
+
+var request = GetRequest();         // A dictionary recording request search information.
+let doc = request.doc ?? "terms";    // The document to be displayed.
+let lang = request.lang ?? "en-us"; // The language to be displayed.
+
+// console.log(`doc: ${doc}, lang: ${lang}`);
+
+function makeSearch(new_doc = doc, new_lang = lang) {
+  location.search = `?doc=${new_doc}&lang=${new_lang}`
+}
 
 // Function to convert Markdown to HTML
 function markdownToHtml(markdown) {
   let html = markdown;
 
   // Convert headers
-  html = html.replace(/^#\s+(.*$)/gm, ""); // Discard H1
+  html = html.replace(/^#\s+(.*$)/gm, "<h2>$1</h2>"); // H1 to <h2>
   html = html.replace(/^##\s+(.*$)/gm, "<h4>$1</h4>"); // H2 to <h4>
   html = html.replace(/^###\s+(.*$)/gm, "<h5>$1</h5>"); // H3 to <h5>
 
@@ -30,7 +58,7 @@ function markdownToHtml(markdown) {
 }
 
 // Read Markdown file
-const filePath = "./docs/terms_en-us.md"; // Replace with your Markdown file path
+const filePath = `./docs/${doc}_${lang}.md`; // Replace with your Markdown file path
 fetch(filePath)
   .then((res) => res.text())
   .then((text) => {
@@ -38,5 +66,5 @@ fetch(filePath)
     const htmlContent = markdownToHtml(text);
 
     // Write the HTML content.
-    document.getElementById("whitepaper-paragraphs").innerHTML = htmlContent;
+    document.getElementById("paragraphs").innerHTML = htmlContent;
   });
