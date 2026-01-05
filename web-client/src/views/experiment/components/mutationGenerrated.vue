13027c80f2ee29d18c0ceac8cd542face6e22460
QZ Shao <shaoqz@icloud.com>
2026-01-04 20:42:39 -0600

    Update: fix wording

--- web-client/src/views/experiment/components/mutationGenerrated.vue
+++ web-client/src/views/experiment/components/mutationGenerrated.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <a-flex justify="space-between">
-      <div class="mutation-title">Mutation generated</div>
+      <div class="mutation-title">Mutant generated</div>
       <DownloadOutlined class="download-icon" @click="handleDownload"  v-if="showDownload"/>
       </a-flex>
     <a-row class="mutation-header">
       <a-col :span="8">Number</a-col>
-      <a-col :span="16">Pattern</a-col>
+      <a-col :span="16">Name</a-col>
     </a-row>
     <div class="mutation-list" v-if="mutations.length">
       <a-row v-for="(item, index) in mutations" class="item">
